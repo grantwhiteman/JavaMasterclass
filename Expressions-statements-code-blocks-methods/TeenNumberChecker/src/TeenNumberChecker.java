d947c85ed2995d1b051d80b0e08414d167256cbe
Grant Whiteman <grantwhiteman95@gmail.com>
2022-01-25 19:47:24 +0000

    coding exercise 7

--- Expressions-statements-code-blocks-methods/TeenNumberChecker/src/TeenNumberChecker.java
+++ Expressions-statements-code-blocks-methods/TeenNumberChecker/src/TeenNumberChecker.java
@@ -1,3 +1,13 @@
 public class TeenNumberChecker {
+    public static boolean hasTeen(int val1, int val2, int val3) {
+        //returns true if values contain a teen number
+        return (12 < val1 && val1 < 20) ||
+                (12 < val2 && val2 < 20) ||
+                (12 < val3 && val3 < 20);
 
+    }
+    public static boolean isTeen(int val1) {
+        //returns true if value is a teen number
+        return (12 < val1 && val1 < 20);
+    }
 }
